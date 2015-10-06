6f08301a65322e6471d234e9b666dccb66571257
MPatel98637 <MPatel@my.harrisburgu.edu>
2015-10-06 19:02:18 -0400

    New File Added
    None

---
+++ Click_For_Student_Info.php
@@ -0,0 +1,33 @@
+<html>
+ <head>
+ </head>
+ <body>
+  <script>
+   var name="Monika Patel";
+   var Address="New Jersey";
+   var Back_Ground="BSc In Commerse, MBA in Finance";
+   var Hobby="Music, Movies, Travell";
+   var result=true;
+
+   document.write("<h1>Student Info</h1>");
+   document.write("<h6>As requested</h6>");
+
+   document.write("Today is " + Date());
+   //concate two string (String+variable)
+
+   document.write("<br/><br/>Name :  "+name);
+   document.write("<br/>Type :  "+typeof(name));
+
+   document.write("<br/><br/>Address :  "+Address);
+   document.write("<br/>Type :  "+typeof(Address));
+
+   document.write("<br/><br/>Back_Ground :  "+Back_Ground);
+   document.write("<br/>Type :  "+typeof(Back_Ground));
+
+   document.write("<br/><br/>Hobby Marks :  "+Hobby);
+   document.write("<br/>Type :  "+typeof(Hobby));
+
+   document.write("<br/>Type :  "+typeof(result));
+  </script>
+ </body>
+ </html>
